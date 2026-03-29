3528e9e64d72bb31ca2a895736a764acc097f322
hoffingson industries <96163218+hoffingsonindustries@users.noreply.github.com>
2026-03-29 20:24:41 +1100

    Update index.js

--- index.js
+++ index.js
@@ -236,7 +236,9 @@ async function registerCommands() {
   }
 }
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect('mongodb://localhost:27017', {
+  dbName: 'AttendanceData'
+});
   .then(() => console.log("Connected to MongoDB Database"))
   .catch((err) => console.error("Could not connect to MongoDB:", err));
 client.once("ready", async () => {
